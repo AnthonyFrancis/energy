fe7ec67973ac5e04543da2ca13b99116d5698723
Anthony Francis <anthony@getpeekapp.com>
2013-12-29 22:40:27 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Energy::Application.config.secret_key_base = '6b8aba4babd88b63935cc71cd75884335f729cd8c9e5c4281546b5cbf9b4836118b61d9446739ec82eac4a83ef54f47eab48c560a670371c7f10c91739bb8274'
