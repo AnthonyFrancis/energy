7d880a77c226c31565369a095685896c73c50f64
Anthony Francis <anthony@getpeekapp.com>
2014-01-06 14:57:27 +0000

    changed colour of 2nd nav bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   def home
   end
 
+  def products
+  end
+
+  def buy
+  end
+
+  def contact
+  end
+
   def order
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Energy::Application.routes.draw do
   root "pages#home"
   get "order" => "pages#order"
+  get "products" => "pages#products"
+  get "buy" => "pages#buy"
+  get "contact" => "pages#contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
