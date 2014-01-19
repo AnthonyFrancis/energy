45f282b758a8010f787a40e782946f7f5232aec4
Anthony Francis <anthony@getpeekapp.com>
2014-01-19 18:52:12 +0000

    changed email address back

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationsMailer < ActionMailer::Base
 
   default :from => "noreply@energyborninafrica.com"
-  default :to => "anthony@getpeekapp.com"
+  default :to => "crocery@gmail.com"
 
   def new_message(message)
     @message = message
